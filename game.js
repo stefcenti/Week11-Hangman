4d345fc125eb54319b59b11bbb846ad05674862a
Stefanie Centi <stefcenti@centiumtech.com>
2016-07-14 17:47:28 -0400

    random word generator in game.js used by word.js; Need to move functionailty from word to letter.js

---
+++ game.js
@@ -0,0 +1,6 @@
+exports.game = {
+	wordlist: ["javascript", "nodejs", "mysql", "mongodb", 
+		"webdesign", "coder"],
+
+	getWord: function() { return this.wordlist[1]; }
+}
---
+++ letter.js
@@ -0,0 +1,9 @@
+/****Control whether or not a letter appears as a "_" 
+/* or as itself on-screen.
+*/
+
+function letter() = {
+	function isBlank() {
+		return true;
+	}
+}
